87c5e2e7844429246aa6e51070327f2fab90e802
jarlissonmoreira <jarlisson.moreira@gmail.com>
2024-12-09 12:55:58 -0300

    Create Primo.py
    O código abaixo verifica se um número maior que 2 é primo ou não.
    
    Check if a given number, greater than 2, is prime or not
    
    Jarlisson Moreira, www.pythonprogressivo.net

---
+++ Algoritmos/Números-Primos/Primo.py
@@ -0,0 +1,17 @@
+# O código abaixo verifica se um número maior que 2 é primo ou não
+# Jarlisson Moreira, www.pythonprogressivo.net
+
+import math
+
+x = int(input("Insira um número maior que 2: "))
+
+is_prime = True
+
+for i in range(2,x):
+  if( x % i == 0):
+    is_prime = False
+
+if is_prime:
+  print(x, "é primo.")
+else:
+  print(x, "não é primo.")
