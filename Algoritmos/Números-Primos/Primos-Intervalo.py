2ab3b6aeca79e91b539976d27a46855b1e600efb
Jarlisson Moreira <jarlisson.moreira@gmail.com>
2024-12-09 13:33:56 -0300

    Update Primos-Intervalo.py

--- Algoritmos/Números-Primos/Primos-Intervalo.py
+++ Algoritmos/Números-Primos/Primos-Intervalo.py
@@ -2,6 +2,8 @@
 - Escreva um programa que exibe todos os números primos entre 1 e N, com N um inteiro positivo fornecido pelo usuário.
 Vamos usar a função prime(x) para verificar se x é primo ou não (ver Primo.py).
 Faremos um looping percorrendo o intervalo 2 até (x-1), e testando se cada um desses valores é primo
+
+Jarlisson Moreira, www.pythonprogressivo.net
 '''
 
 def prime(x):
