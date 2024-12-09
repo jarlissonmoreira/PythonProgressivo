6bbdca4e4b56d7839cd0d423e40e4d8585a2fb4d
Jarlisson Moreira <jarlisson.moreira@gmail.com>
2024-12-09 13:33:35 -0300

    Create Primos-Intervalo.py
    Script em Python que exibe todos os números primos entre 1 e um número inteiro positivo N fornecido.
    
    Python script that displays all prime numbers between 1 and a given positive integer N.
    
    Jarlisson Moreira, www.pythonprogressivo.net

---
+++ Algoritmos/Números-Primos/Primos-Intervalo.py
@@ -0,0 +1,24 @@
+'''
+- Escreva um programa que exibe todos os números primos entre 1 e N, com N um inteiro positivo fornecido pelo usuário.
+Vamos usar a função prime(x) para verificar se x é primo ou não (ver Primo.py).
+Faremos um looping percorrendo o intervalo 2 até (x-1), e testando se cada um desses valores é primo
+'''
+
+def prime(x):
+  is_prime = True
+
+  for i in range(2,x):
+    if( x % i == 0):
+      is_prime = False
+
+  return is_prime
+
+x = int(input("Insira um número inteiro positivo: "))
+
+primos = []
+
+for i in range(2,x):
+  if( prime(i) == True):
+    primos.append(i)
+
+print("Primos existentes entre 1 e ",x,": ", primos)
